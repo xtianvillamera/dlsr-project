05688400802b4d5d7f79e90c7173e6c9ec621f04
xtianvillamera <villamera.christian@gmail.com>
2019-02-22 07:59:29 +0800

    users app have been created

--- dlsr/settings.py
+++ dlsr/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'viewing.apps.ViewingConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'viewing-ViewingDegProgList'
+LOGIN_URL = 'login'
